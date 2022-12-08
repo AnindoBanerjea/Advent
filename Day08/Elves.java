2de78588df7a675a47571e7828b4bcf7f2a728b9
Anindo Banerjea <banerjea@gmail.com>
2022-12-08 13:16:26 -0800

    Removed hardcoding to part2

--- Day08/Elves.java
+++ Day08/Elves.java
@@ -1,6 +1,5 @@
 public class Elves {
     public static void main(String[] args) {
-        args = new String[]{"2"};
         if (args.length == 0 || !args[0].matches("^\\d+$")) {
             System.out.println("Give 1 or 2 as the argument, based on which part you are solving.");
         } else {
