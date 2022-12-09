ead1814c29901ea0dcff79309a7baface0d3cd19
Anindo Banerjea <banerjea@gmail.com>
2022-12-08 17:46:27 -0800

    Added more comments

--- Day08/Forest.java
+++ Day08/Forest.java
@@ -52,18 +52,27 @@ public class Forest {
     }
 
     private void setVisible(boolean[][] visible, int[] dir){
+        // Generic loop that can go right to left, left to right, bottom to top or top to bottom through the array
+        // Outer loop can be for rows or colums (we will swap using ii and jj later), but it is always forward
         for (int i = 0; i < ((dir[0] == 0) ? trees[0].length : trees.length); i++){
             int tallest = -1;
-            for (int j = ((dir[0] == -1) ? trees.length -1 : ((dir[1] == -1) ? trees[0].length -1 : 0));
+            for (
+                 // inner loop can be backwards if direction is negative, if it is backwards, start from 
+                 // tree.length - 1 or tree[0].length - 1 depending on direction. Else start from 0.
+                 int j = ((dir[0] == -1) ? trees.length -1 : ((dir[1] == -1) ? trees[0].length -1 : 0));
+                 // termination condition is written to be able to handle either direction of travel
                  ((j >= 0) && (j < ((dir[0] == 0) ? trees[0].length : trees.length)));
+                 // since exactly one of dir[0] or dir[1] is non-zero, we can just add them to get the
+                 // direction of travel
                  j += (dir[0] + dir[1])) {
-                int pointerx = ((dir[0] == 0) ? i : j );
-                int pointery = ((dir[0] == 0) ? j : i );
-                if (trees[pointerx][pointery] > tallest) {
-                    visible[pointerx][pointery] = true;
-                    tallest = trees[pointerx][pointery];
+                // if we are moving horizontally, keep the order. If we are moving vertically, swap i and j 
+                int ii = ((dir[0] == 0) ? i : j );
+                int jj = ((dir[0] == 0) ? j : i );
+                if (trees[ii][jj] > tallest) {
+                    visible[ii][jj] = true;
+                    tallest = trees[ii][jj];
                 } else {
-                    visible[pointerx][pointery] = false;
+                    visible[ii][jj] = false;
                 }
             }
         }            
@@ -103,16 +112,26 @@ public class Forest {
 
     public int viewingScore(int x, int y, int[] dir){
         int score = 0;
+        // Generic loop that can go right to left, left to right, bottom to top or top to bottom through the array
+        // if looking up or down, intialize ij (because it can represent either i or j) to x or y (depending on
+        // whethere we are doing horizontally or vertitally) + or - 1 depending on the direction. Since only one
+        // of dir[0] or dir[1] can be non-zero, we just add both to get the +-1
         for (int ij = ((dir[0] == 0) ? y : x) + dir[0] + dir[1];
-            ((ij >= 0) && (ij < ((dir[0] == 0) ? trees[0].length : trees.length)));
-              ij += (dir[0] + dir[1])){
-            int pointerx = (dir[0] == 00) ? x : ij;
-            int pointery = (dir[0] == 0) ? ij : y;
-            if (trees[pointerx][pointery] >= trees[x][y] ||
+             // The termination condition handles both zero and tree length
+             ((ij >= 0) && (ij < ((dir[0] == 0) ? trees[0].length : trees.length)));
+             // for the increment, since only one of dir[0] or dir[1] can be non-zero, we just add both
+             ij += (dir[0] + dir[1])){
+            // Now, based on whether we are going horizontally (dir[0] == 0), or vertitally (otherwise)
+            // we use ij to move the appropriate index and keep the other one constant at x or y
+            int ii = (dir[0] == 00) ? x : ij;
+            int jj = (dir[0] == 0) ? ij : y;
+            if (trees[ii][jj] >= trees[x][y] ||
                 ij == 0 ||
                 ij == ((dir[0] == 0) ? trees[0].length-1 : trees.length -1)) {
-                score = dir[1]*(ij-y) + dir[0]*(ij-x);
-                break;
+                    // multiplying by dir[0] and dir[1] eliminates the score for the direction we are not looking
+                    // as well as flips the sign in the case direction is -1
+                    score = dir[1]*(ij-y) + dir[0]*(ij-x);
+                    break;
             }
         }
         return score;
@@ -135,5 +154,4 @@ public class Forest {
         }
         return topscore;
     }
-
 }
